ac61700fbae532607c1c146fb0b87933af8c1ec8
Kirill <kirrilf@mail.ru>
2021-02-20 19:12:27 +0300

    complete 3 and 4 points

--- src/main/java/org/kirrif/controller/CarController.java
+++ src/main/java/org/kirrif/controller/CarController.java
@@ -13,14 +13,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
-@RestController
+@RestController()
+@RequestMapping(value = "/api")
 public class CarController {
 
     private final List<Car> cars;
@@ -32,19 +33,19 @@ public class CarController {
     }
 
 
-    @GetMapping("/cars")
+    @GetMapping(value = "/cars")
     public ResponseEntity<String> getCar(@RequestParam Optional<String> country,
-                                 @RequestParam Optional<String> segment,
-                                 @RequestParam Optional<Double> minEngineDisplacement,
-                                 @RequestParam Optional<Integer> minEngineHorsepower,
-                                 @RequestParam Optional<Integer> minMaxSpeed,
-                                 @RequestParam Optional<String> search,
-                                 @RequestParam Optional<Boolean> isFull,
-                                 @RequestParam Optional<Integer> year,
-                                 @RequestParam Optional<String> bodyStyle) throws JsonProcessingException {
+                                         @RequestParam Optional<String> segment,
+                                         @RequestParam Optional<Double> minEngineDisplacement,
+                                         @RequestParam Optional<Integer> minEngineHorsepower,
+                                         @RequestParam Optional<Integer> minMaxSpeed,
+                                         @RequestParam Optional<String> search,
+                                         @RequestParam Optional<Boolean> isFull,
+                                         @RequestParam Optional<Integer> year,
+                                         @RequestParam Optional<String> bodyStyle) throws JsonProcessingException {
 
         List<Car> carsAfterFilters = cars.stream()
-                .filter(car -> !country.isPresent() || country.get().equals(CarService.getCarBrand(car, brands).getCountry()))
+                .filter(car -> !country.isPresent() || country.get().equals(Objects.requireNonNull(CarService.getCarBrand(car, brands)).getCountry()))
                 .filter(car -> !segment.isPresent() || segment.get().equals(car.getSegment()))
                 .filter(car -> !minEngineDisplacement.isPresent() || minEngineDisplacement.get() * 1000 <= car.getEngine().getEngine_displacement())
                 .filter(car -> !minEngineHorsepower.isPresent() || minEngineHorsepower.get() <= car.getEngine().getEngine_horsepower())
@@ -54,11 +55,11 @@ public class CarController {
                 .filter(car -> !year.isPresent() || CarService.isReleasedThatYear(car, year.get()))
                 .collect(Collectors.toList());
 
-        if(carsAfterFilters.size() == 0){
+        if (carsAfterFilters.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        List<CarRespondDto> carRequestDtoList = carsAfterFilters.stream().map(car -> CarRespondDto.fromCar(car, CarService.getCarBrand(car, brands))).collect(Collectors.toList());
+        List<CarRespondDto> carRequestDtoList = carsAfterFilters.stream().map(car -> CarRespondDto.fromCar(car, Objects.requireNonNull(CarService.getCarBrand(car, brands)))).collect(Collectors.toList());
 
         ObjectMapper mapper = new ObjectMapper();
 
@@ -78,9 +79,80 @@ public class CarController {
     }
 
 
-    @GetMapping("brands")
-    public List<Brand> getBrands() {
-        return Cars.getBrands();
+    @GetMapping(value = "/fuel-types")
+    public Set<String> getFuelTypes() {
+        Set<String> fuelTypes = new HashSet<>();
+        for (Car car : cars) {
+            fuelTypes.add(car.getEngine().getEngine_type());
+        }
+        return fuelTypes;
+    }
+
+    @GetMapping(value = "/body-styles")
+    public Set<String> getBodyStyles() {
+        Set<String> bodyStyles = new HashSet<>();
+        for (Car car : cars) {
+            bodyStyles.add(car.getBody().getBody_style());
+        }
+        return bodyStyles;
+    }
+
+    @GetMapping(value = "/wheel-drives")
+    public Set<String> getWheelDrives() {
+        Set<String> wheelDrives = new HashSet<>();
+        for (Car car : cars) {
+            wheelDrives.add(car.getWheel_drive());
+        }
+        return wheelDrives;
+    }
+
+    @GetMapping(value = "/gearboxes")
+    public Set<String> getGearboxes() {
+        Set<String> gearboxes = new HashSet<>();
+        for (Car car : cars) {
+            gearboxes.add(car.getGearbox());
+        }
+        return gearboxes;
+    }
+
+    @GetMapping(value = "/max-speed")
+    public ResponseEntity<Integer> getAverageMaxSpeed(@RequestParam Optional<String> brand,
+                                                     @RequestParam Optional<String> model) {
+        if (brand.isPresent() && model.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        int sumSpeed = 0, count = 0;
+        if (brand.isPresent()) {
+            for (Car car : cars) {
+                if (brand.get().equals(Objects.requireNonNull(CarService.getCarBrand(car, brands)).getTitle())) {
+                    sumSpeed += car.getMax_speed();
+                    count++;
+                }
+            }
+            if (count == 0) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }else {
+                return new ResponseEntity<>(sumSpeed/count, HttpStatus.OK);
+            }
+        }
+        if (model.isPresent()) {
+            for (Car car : cars) {
+                if (model.get().equals(car.getModel())) {
+                    sumSpeed += car.getMax_speed();
+                    count++;
+                }
+            }
+            if (count == 0) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }else {
+                return new ResponseEntity<>(sumSpeed/count, HttpStatus.OK);
+            }
+        }
+
+
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
     }
 
 
